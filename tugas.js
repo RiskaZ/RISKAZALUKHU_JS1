1474a59e552ae59c5c0e55e93b97934daac91974
RiskaZ <riskazalukhu349@gmail.com>
2023-10-13 22:47:04 +0700

    first commit

---
+++ tugas.js
@@ -0,0 +1,51 @@
+//if else
+var jenkel = perempuan;
+
+if(jenkel=perempuan) {
+    console.log ('Hallo Miss' );
+}else{
+    console.log ('Hallo');
+}
+
+//switch case
+
+var z= "0";
+switch (z) {
+  case 0:
+    text = "ya";
+    break;
+  case 1:
+    text = "no";
+    break;
+  default:
+    text = "Nilai tidak valid";
+}
+
+//for statement
+var i="saya" ;
+
+for (var i = saya; i <=6 ; i++) {
+    console.log ('Hallo' + i );
+  }
+
+  //while 
+  
+while (buah<=10) {
+    console.log ('buah' + apel);
+    buah++;
+}
+
+  //do while
+
+  do {
+    console.log ('buah' + apel);
+    i++;
+  }
+  while (buah < 10);
+
+  //function
+  let x = angka(10, 5);
+  function angka(a, b) {
+    return a * b;
+  }
+
